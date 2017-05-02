affd2d00caaf70e312f58b9a4c63a2ed28a6c0ea
Phoenix Yu <phoenix.yu@testsys.com>
2017-05-02 16:01:59 -0400

    Added support for sending private message to a user.

---
+++ src/HipChat.Net/Models/Request/SendMessage.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace HipChat.Net.Models.Request
+{
+
+    [JsonObject]
+    public class SendMessage
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("notify")]
+        public bool Notify { get; set; }
+
+        [JsonProperty("message_format"), JsonConverter(typeof(StringEnumConverter))]
+        public MessageFormat Format { get; set; }
+    }
+}
